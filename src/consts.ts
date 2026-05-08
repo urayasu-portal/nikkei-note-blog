98c8295b82321b8f152351db0cf368408d54213a
urayasu-portal <urayasu.portal@gmail.com>
2026-05-09 06:53:30 +0900

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "本日の日本株ノート";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "毎日の日本株市場を、むずかしすぎず・軽すぎず整理する相場ノート";
