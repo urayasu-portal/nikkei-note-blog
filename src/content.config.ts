39a3a90bef9263068519a97d6ddca8355923e533
urayasu-portal <urayasu.portal@gmail.com>
2026-05-10 07:02:57 +0900

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,16 +3,24 @@ import { defineCollection } from "astro:content";
 import { z } from "astro/zod";
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
 	loader: glob({ base: "./src/content/blog", pattern: "**/*.{md,mdx}" }),
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
+		category: z.string().optional(),
+		nikkei: z.string().optional(),
+		nikkeiDate: z.string().optional(),
+		topix: z.string().optional(),
+		topixDate: z.string().optional(),
+		usd: z.string().optional(),
+		usdDate: z.string().optional(),
+		dow: z.string().optional(),
+		dowDate: z.string().optional(),
+		nasdaq: z.string().optional(),
+		nasdaqDate: z.string().optional(),
 	}),
 });
 
