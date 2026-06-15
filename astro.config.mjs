698c6bffb92723eb2f1c28db110cfbec93a8faed
nikkei-note <nikkei-note@noreply.github.com>
2026-06-15 22:34:08 +0900

    Fix: Search Console URL正規化対応

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import cloudflare from "@astrojs/cloudflare";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://nikkei-note.com",
+	trailingSlash: "always",
 	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
